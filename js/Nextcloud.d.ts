617b65d357dd6952b65003422b89ebf645a79413
Robin Appelman <robin@icewind.nl>
2018-08-16 18:27:31 +0200

    update nextcloud.d.ts
    Signed-off-by: Robin Appelman <robin@icewind.nl>

--- js/Nextcloud.d.ts
+++ js/Nextcloud.d.ts
@@ -21,6 +21,22 @@ declare namespace OC {
 		function filepicket(title: string, callback: (result: string | string[]) => void, multiselect?: boolean, mimetypeFilter?: string, modal?: boolean): void;
 	}
 
+	interface Plugin<T> {
+		name: string;
+		attach: (instance: T, options: any) => void;
+		detach?: (instance: T, options: any) => void;
+	}
+
+	namespace Plugins {
+		function register<T>(scope: string, plugin: OC.Plugin<T>): void;
+	}
+
+	namespace Search {
+		interface Core {
+			setFilter: (app: string, callback: (query: string) => void) => void;
+		}
+	}
+
 	function generateUrl(url: string, parameters?: { [key: string]: string }, options?: EscapeOptions)
 
 	function linkToOCS(service: string, version: number): string;
@@ -37,8 +53,18 @@ declare namespace OC {
 
 declare function t(app: string, string: string, vars?: { [key: string]: string }, count?: number, options?: EscapeOptions): string;
 
-declare function humanFileSize(size: number): string;
-declare function relative_modified_date(mtime: number): string;
+declare const oc_config: {
+	blacklist_files_regex: string;
+	enable_avatars: boolean;
+	last_password_link: string | null;
+	modRewriteWorking: boolean;
+	session_keepalive: boolean;
+	session_lifetime: boolean;
+	"sharing.maxAutocompleteResults": number;
+	"sharing.minSearchStringLength": number;
+	version: string;
+	versionString: string;
+};
 
 declare module 'NC' {
 	export interface OCSResult<T> {
